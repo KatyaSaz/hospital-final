50562cd34d62c6849c4f14901d376a4ee65be97d
Katya Sazonova <45514862+KatyaSaz@users.noreply.github.com>
2020-12-15 23:34:56 +0200

    add UserService

---
+++ src/main/java/ua/sazonova/hospital/service/UserService.java
@@ -0,0 +1,27 @@
+package ua.sazonova.hospital.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ua.sazonova.hospital.entity.User;
+import ua.sazonova.hospital.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(()->new UsernameNotFoundException("User doesn't exist"));
+    }
+
+    public void save(User user){
+        userRepository.save(user);
+    }
+}
